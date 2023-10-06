ce230ebc67a992cb1d2c71d293060c75b89e62ad
dharmender0801 <teamsoul234@gmail.com>
2023-10-06 11:03:17 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function App() {
   return (
     <div className="App">
      <h1>This Is checking</h1>
-     <h1>AWS</h1>
-     <h2>This is for testing </h2>
-     <h1>AWS1111</h1>
+
     </div>
   );
 }
