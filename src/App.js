0a0dc2bdcb698543bacbe0ee17f5b27104807824
dharmender0801 <teamsoul234@gmail.com>
2023-10-07 13:00:29 +0530

    workign on question page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './components/Home';
 import LoginSignup from './components/LoginSignup';
+import Question from './components/Question';
 
 function App() {
   return (
     <div className="App">
       <LoginSignup />
+      <Question />
       <Home />
       <h1>Aws</h1>
     </div>
