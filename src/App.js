de3a56d8495084a86b89d44bd470dad3f65dedea
Deepak1999 <deepakmuz151@gmail.com>
2023-10-07 12:41:06 +0530

    project structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Home from './components/Home';
+import LoginSignup from './components/LoginSignup';
 
 function App() {
   return (
     <div className="App">
+      <LoginSignup />
+      <Home />
       <h1>Aws</h1>
     </div>
   );
