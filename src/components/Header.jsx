84c4ef27decb128607eea163ee4dcb99fae724d1
Deepak1999 <deepakmuz151@gmail.com>
2023-11-01 12:06:35 +0530

    testing for new updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>E-COMMERCE </span>
+            <span className='text-slate-500'>E-COMMERCE update on 01 nov 2023 </span>
 
           </h1>
         </Link>
